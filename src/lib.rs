08cecf18b1f65a5eef0c496aac6129454c0de04b
David Gelkin <david.gelkin@gmail.com>
2018-08-06 22:24:54 +0300

    removed duplicate code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 pub fn is_leap_year(year: i32) -> bool {
 
-    (year % 4 == 0)  && (year % 100 != 0) ||
-    ((year % 4 == 0)  && (year % 100 != 0) || (year % 400 == 0))
-    
+    (year % 4 == 0)  && (year % 100 != 0) || (year % 400 == 0)
+
 }
